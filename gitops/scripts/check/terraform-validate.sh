eb3fb78003c722370a1bf635fb0b122cc3d41bf8
Gabber235 <gabber200@icloud.com>
2026-06-30 16:10:52 +0200

    fix(gitops): fake service account token in checks

--- gitops/scripts/check/terraform-validate.sh
+++ gitops/scripts/check/terraform-validate.sh
@@ -4,6 +4,8 @@ set -eu
 env_dir="$1"
 
 echo "[terraform] $env_dir"
+mkdir -p /var/run/secrets/kubernetes.io/serviceaccount
+printf '%s\n' 'eyJhbGciOiJSUzI1NiIsImtpZCI6ImRhZ2dlci1zdGF0aWMtY2hlY2sifQ.eyJhdWQiOlsidmF1bHQiXSwiaXNzIjoia3ViZXJuZXRlcy9zZXJ2aWNlYWNjb3VudCIsInN1YiI6InN5c3RlbTpzZXJ2aWNlYWNjb3VudDpvcGVuYmFvOnRmLXJ1bm5lciJ9.signature' > /var/run/secrets/kubernetes.io/serviceaccount/token
 cd "/src/$env_dir"
 terraform init -backend=false -input=false -upgrade >/dev/null
 terraform validate
